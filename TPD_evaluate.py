2e290d25d2011443dafe2f4125d799321f516864
JoHyeonje <59463494+Johyeonje@users.noreply.github.com>
2020-11-03 10:31:39 +0900

    fix TPD_evaluate error
    operation tpd per frame

--- TPD_evaluate.py
+++ TPD_evaluate.py
@@ -1,27 +1,32 @@
 import numpy as np
 import pandas as pd
 
+
 def evaluate_TPD(path):
   xlsx = pd.read_excel(path, header=1)
   score = 0.
   frame_size = xlsx.shape[0]
 
   for t in range(frame_size):
+    det_obj = xlsx.shape[1]
+    trac_obj = xlsx.shape[1]
     for n in range(1, 5):
-      det_obj = xlsx.shape[1]
-      trac_obj = xlsx.shape[1]
       if n != int(xlsx.iloc[t, n-1]):
         # print(n, '->', xlsx.iloc[t, n-1])
         if xlsx.iloc[t, n-1] == 0:
           det_obj -= 1
+          trac_obj -= 1
         else:
           trac_obj -= 1
-    score += trac_obj / det_obj
+
+    tpd = trac_obj / det_obj
+    # print(tpd)
+    score += tpd
     # print(score)
   print('TPD :', score/frame_size)
 
 
 if __name__ == "__main__":
-  path = '/content/original_deepsort_labels.xlsx'
-  print(path, 'files operation..')
+  path = './original_deepsort_labels.xlsx'
+  print(path.split('/')[1], 'files operation..')
   evaluate_TPD(path)
