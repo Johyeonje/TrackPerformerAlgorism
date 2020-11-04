b03347503eb789fce7315173d2b3b5eac0748837
JoHyeonje <59463494+Johyeonje@users.noreply.github.com>
2020-11-04 11:03:52 +0900

    Update object_tracker_cus2.py

--- object_tracker_cus2.py
+++ object_tracker_cus2.py
@@ -296,16 +296,16 @@ class Apply_Models(object):
                 print(nmtX)
 
                 # Apply center location Euclidean Distance
-                if not self.person1.is_used:
+                if not self.person1.is_used and self.person1.centerX == None:
                     gap = np.sqrt(pow(self.person1.centerX-nmtX, 2)+pow(self.person1.centerY-nmtY, 2))
                     compare_list.append([1, tmp[0], gap])
-                if not self.person2.is_used:
+                if not self.person2.is_used and self.person2.centerX == None:
                     gap = np.sqrt(pow(self.person2.centerX-nmtX, 2)+pow(self.person2.centerY-nmtY, 2))
                     compare_list.append([2, tmp[0], gap])
-                if not self.person3.is_used:
+                if not self.person3.is_used and self.person3.centerX == None:
                     gap = np.sqrt(pow(self.person3.centerX-nmtX, 2)+pow(self.person3.centerY-nmtY, 2))
                     compare_list.append([3, tmp[0], gap])
-                if not self.person4.is_used:
+                if not self.person4.is_used and self.person4.centerX == None:
                     gap = np.sqrt(pow(self.person4.centerX-nmtX, 2)+pow(self.person4.centerY-nmtY, 2))
                     compare_list.append([4, tmp[0], gap])
 
